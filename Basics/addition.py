0e56a9722c015fd77c3f2e8073ed1ff468315681
Vaishnavinikam23 <nikamvaishnavi2344@gmail.com>
2023-08-23 18:53:24 +0530

    Basics

---
+++ Basics/addition.py
@@ -0,0 +1,14 @@
+
+a=int(input("Enter any number:"))
+b=a//1000
+print(b)
+c=a%1000
+d=c//100
+print(d)
+e=c%100
+f=e//10
+print(f)
+g=e%10
+print(g)
+z=b+d+f+g
+print(z)
---
+++ Basics/hello.py
@@ -0,0 +1,17 @@
+
+x=100
+y=1.2
+z="Hello"
+arr=[10,20,30]
+t1=(10,20,30)
+d1={"id":"100","Name":"abc"}
+s1={10,20,30}
+b1=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(arr))
+print(type(t1))
+print(type(d1))
+print(type(s1))
+print(type(b1))
