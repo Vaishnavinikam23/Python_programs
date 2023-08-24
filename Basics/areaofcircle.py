7c37cb62cc644c991bb13f0fe3c7b93912c4cd51
Vaishnavinikam23 <nikamvaishnavi2344@gmail.com>
2023-08-24 20:13:14 +0530

    area

---
+++ Basics/areaofcircle.py
@@ -0,0 +1,6 @@
+a=int(input("Enter radius of circle"))
+area=3.14*a**2
+circumference=2*3.14*a
+print(area)
+print(circumference)
+
---
+++ Basics/gross income.py
@@ -0,0 +1,7 @@
+a=int(input("Enter basic salary:"))
+da=(20/100)*a
+hra=(40/100)*a
+grossincome = a+da+hra
+print(da)
+print(hra)
+print(grossincome)
